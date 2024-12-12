1f58114102741a62afd83acf97bd0e98dc64980c
garvsl <garvsehgal7@gmail.com>
2024-12-12 15:41:08 -0500

    swap to jsdom

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { JSDOM } from "jsdom";
+
 async function main(user) {
   const headers = {
     cache: "default",
@@ -18,57 +20,87 @@ async function main(user) {
   try {
     const response = await fetch(`https://devpost.com/${user}`, headers);
     const result = await response.text();
-    const split = result.split(`data-software-id`);
-    split.shift();
-    const hackathons = split.map(async (e) => {
-      const hack = e.slice(0, e.indexOf("<!-- cache end -->"));
 
-      let title = hack.indexOf("<h5");
-      title = hack
-        .slice(hack.indexOf(">", title) + 1, hack.indexOf("</h5>", title))
-        .trim();
+    const dom = new JSDOM(result);
 
-      let tag = hack.indexOf("<p");
-      tag = hack
-        .slice(hack.indexOf(">", tag) + 1, hack.indexOf("</p>", tag))
-        .trim();
+    let hackathons_split =
+      dom.window.document.querySelectorAll("[data-software-id]");
+    let res = {
+      username: "",
+      total: 0,
+      wins: 0,
+      hackathons: [],
+    };
 
-      let link = e.indexOf("href");
-      link = e.slice(link + 6, e.indexOf(`">`, link + 5));
+    res["username"] = user;
 
-      let image = hack.indexOf(`src="`);
-      image = hack.slice(image + 5, hack.indexOf("/>", image) - 2);
-
-      const winner = hack.includes("Winner");
-      let wins = new Promise(async (resolve, reject) => {
-        if (winner) {
-          const software = await fetch(link, headers);
-          const software_result = await software.text();
-          let awards = software_result.split("Winner");
-          awards.shift();
-          awards = awards.map((win) => {
-            const award_name = win.slice(8, win.indexOf("</li>")).trim();
-            return award_name.replace(/&quot;/g, '"');
-          });
-          resolve(awards);
-        } else {
-          resolve([]);
-        }
-      });
-
-      return {
-        id: e.slice(2, e.indexOf(`">`)),
-        link,
-        title,
-        tag,
-        image,
-        // TODO tech stack
-        winner,
-        awards: await wins,
-      };
+    hackathons_split.forEach((hackathon, i, theArray) => {
+      const id =
+        hackathon.attributes.getNamedItem("data-software-id").textContent;
+      const title = hackathon.querySelector("h5").textContent.trim();
+      const link = hackathon.querySelector("a").href;
+      const tag = hackathon.querySelector("p").textContent.trim();
+      const img = hackathon.querySelector("img").src;
+      const winner = hackathon.contains(
+        hackathon.querySelector('img[alt="Winner"]')
+      );
+      res["wins"] += winner ? 1 : 0;
+      res["total"] += 1;
+      res["hackathons"].push({ id, link, title, tag, img, winner });
     });
 
-    return Promise.all(hackathons);
+    return res;
+
+    // split.shift();
+    // const hackathons = split.map(async (e) => {
+    //   const hack = e.slice(0, e.indexOf("<!-- cache end -->"));
+
+    //   let title = hack.indexOf("<h5");
+    //   title = hack
+    //     .slice(hack.indexOf(">", title) + 1, hack.indexOf("</h5>", title))
+    //     .trim();
+
+    //   let tag = hack.indexOf("<p");
+    //   tag = hack
+    //     .slice(hack.indexOf(">", tag) + 1, hack.indexOf("</p>", tag))
+    //     .trim();
+
+    //   let link = e.indexOf("href");
+    //   link = e.slice(link + 6, e.indexOf(`">`, link + 5));
+
+    //   let image = hack.indexOf(`src="`);
+    //   image = hack.slice(image + 5, hack.indexOf("/>", image) - 2);
+
+    //   const winner = hack.includes("Winner");
+    //   let wins = new Promise(async (resolve, reject) => {
+    //     if (winner) {
+    //       const software = await fetch(link, headers);
+    //       const software_result = await software.text();
+    //       let awards = software_result.split("Winner");
+    //       awards.shift();
+    //       awards = awards.map((win) => {
+    //         const award_name = win.slice(8, win.indexOf("</li>")).trim();
+    //         return award_name.replace(/&quot;/g, '"');
+    //       });
+    //       resolve(awards);
+    //     } else {
+    //       resolve([]);
+    //     }
+    //   });
+
+    //   return {
+    //     id: e.slice(2, e.indexOf(`">`)),
+    //     link,
+    //     title,
+    //     tag,
+    //     image,
+    //     // TODO tech stack
+    //     winner,
+    //     awards: await wins,
+    //   };
+    // });
+
+    // return Promise.all(hackathons);
   } catch (e) {
     console.error(e);
   }
